798a8699b80bc57c3761b46c8cf7c3b89f3b8618
Sharath S Rao <sharathsrao2@gmail.com>
2023-05-05 12:40:20 +0530

    “updated_credentails”

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,10 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="######################################",
-        database="######",
-        user="#######",
-        password="################")
+        host="dpg-chaa7oqk728r8862hl60-a.oregon-postgres.render.com",
+        database="todo_1cts",
+        user="root",
+        password="0IaSleSgrM2M1jumKQeV8tGSrnuFM5R0")
 # To prevent from using a blueprint, we use a cyclic import
 # This also means that we need to place this import here
 # pylint: disable=cyclic-import, wrong-import-position
